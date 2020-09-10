a85e04ea5ad651b89e7eb3a0b1806187f8317c09
HaroldCris <haroldcris@aitech.solutions>
2020-09-10 08:52:18 +0800

    Removed: FieldName on EventArgs

--- TrackableObject/TrackablePropertyChangedEventObject.cs
+++ TrackableObject/TrackablePropertyChangedEventObject.cs
@@ -4,14 +4,11 @@ namespace AiTech.Trackable
 {
     public class TrackablePropertyChangedEventObject : PropertyChangedEventArgs 
     {
-        public string FieldName { get; }
-
         public dynamic OldValue { get; }
         public dynamic NewValue { get; }
 
         public TrackablePropertyChangedEventObject(string propertyName, dynamic oldValue, dynamic newValue) : base(propertyName)
         {
-            FieldName = propertyName;
             OldValue  = oldValue;
             NewValue  = newValue;
         }
